2731d76b01d1b2087f4b9b9a998f1251b544cbd5
pinchyCZN <pinchyCZN@gmail.com>
2017-03-08 16:08:07 -0600

    updates

--- VER2/src/main.d
+++ VER2/src/main.d
@@ -1,5 +1,6 @@
 module main;
 
+import core.stdc.stdio: FILE,SEEK_SET,SEEK_END;
 import libc_map;
 import intel_hda;
 import mp3_file;
@@ -60,6 +61,97 @@ int extract_ext(const char *fname,char *ext,int ext_len)
 		ext[ext_len-1]=0;
 	return 0;
 }
+int get_flen(FILE *f)
+{
+	uint offset,len;
+	offset=ftell(f);
+	fseek(f,SEEK_END,0);
+	len=ftell(f);
+	fseek(f,SEEK_SET,offset);
+	return len;
+}
+int seek_char(const char *buf,uint buf_size,char a,ref int offset,int dir)
+{
+	int result=false;
+	while(1){
+		if(offset>=buf_size){
+			if(buf_size!=0)
+				offset=buf_size-1;
+			else
+				offset=0;
+			if(dir>=0)
+				break;
+		}
+		else if(offset<=0){
+			offset=0;
+			if(dir<0)
+				break;
+		}
+		char tmp=buf[offset];
+		if(tmp==a){
+			result=true;
+			break;
+		}
+		if(dir>=0)
+			offset++;
+		else
+			offset--;
+	}
+	return result;
+}
+int seek_line(const char *buf,uint buf_size,int line,ref uint offset)
+{
+	int result=false;
+	int count=0;
+	int found_start=false;
+	while(1){
+		ubyte a;
+		if(offset>=buf_size){
+			if(buf_size!=0)
+				offset=buf_size-1;
+			else
+				offset=0;
+			break;
+		}
+		else if(offset<=0){
+			offset=0;
+		}
+		if(count==line)
+			break;
+		a=buf[offset];
+		if(a=='\n'){
+			
+		}
+	}
+	return 0;
+}
+int process_playlist(const char *fname)
+{
+	FILE *f;
+	char *playlist;
+	uint len;
+	f=fopen(fname,"rb");
+	if(f is null){
+		printf("unable to open file:%s\n",fname);
+		return 0;
+	}
+	len=get_flen(f)+1;
+	playlist=cast(char*)malloc(len);
+	if(playlist !is null){
+		fread(playlist,1,len,f);
+		playlist[len-1]=0;
+	}
+	fclose(f);
+	if(playlist is null){
+		printf("unable to allocate mem for playlist\n");
+		return 0;
+	}
+	uint offset=0,line=0;
+	while(1){
+		seek_line(playlist,len,line,offset);
+		
+	}
+}
 int process_file(const char *fname)
 {
 	char tmp[MAX_PATH];
@@ -68,7 +160,7 @@ int process_file(const char *fname)
 		printf("playing mp3 file:%s\n",fname);
 		play_mp3(fname);		
 	}else if(strstri(tmp.ptr,"txt".ptr)){
-		
+		process_playlist(fname);
 	}
 	return 0;
 }
